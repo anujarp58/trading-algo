b6d1bbddc4822b4e862f714bd7f01e42e716c8ae
Anuja <anujap58@gmail.com>
2023-02-07 15:08:12 +0100

    updated SignalStrategyFactory:to initialize the map lazily on the first call to get the strategy

--- src/main/java/com/trading/algo/service/SignalStrategyFactory.java
+++ src/main/java/com/trading/algo/service/SignalStrategyFactory.java
@@ -1,6 +1,6 @@
 /**
  *
- *This class determines strategy based on signal and returns it.
+ * This class determines strategy based on signal and returns it.
  *
  */
 package com.trading.algo.service;
@@ -10,20 +10,28 @@ import com.trading.algo.strategy.*;
 import java.util.HashMap;
 import java.util.Map;
 public class SignalStrategyFactory {
-    public static final Map<Integer, SignalStrategy> strategyMap = new HashMap<>();
+    private static Map<Integer, SignalStrategy> strategyMap;
 
-    static {
-        strategyMap.put(1, new SetUpSignalStrategy());
-        strategyMap.put(2, new ReverseSignalStrategy());
-        strategyMap.put(3, new PerformCalcStrategy());
-        strategyMap.put(-1, new CancelTradesStrategy());
-        // Add more strategies as needed
-    }
+    //initialize the map lazily on the first call to get the strategy
     public static SignalStrategy getStrategy(int signal) {
+        if (strategyMap == null || strategyMap.isEmpty()) {
+            initMap();
+        }
         SignalStrategy strategy = strategyMap.get(signal);
         if (strategy == null) {
             throw new IllegalArgumentException("Invalid signal: " + signal);
         }
         return strategy;
+    }
+
+    private static synchronized void initMap() {
+        if (strategyMap == null) {
+            strategyMap = new HashMap<>();
+            strategyMap.put(1, new SetUpSignalStrategy());
+            strategyMap.put(2, new ReverseSignalStrategy());
+            strategyMap.put(3, new PerformCalcStrategy());
+            strategyMap.put(-1, new CancelTradesStrategy());
+            // Add more strategies as needed
+        }
     }
 }
