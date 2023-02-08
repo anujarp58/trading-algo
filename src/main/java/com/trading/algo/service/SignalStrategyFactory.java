9ac6fcd54c69484752be03c667e4a99c9ba302ef
Anuja <anujap58@gmail.com>
2023-02-08 15:53:50 +0100

    To scale and maintain new addition of signals 1.added config.properties 2.updated SignalStrategyFactory:to read the map from a configuration file

--- src/main/java/com/trading/algo/service/SignalStrategyFactory.java
+++ src/main/java/com/trading/algo/service/SignalStrategyFactory.java
@@ -7,15 +7,18 @@ package com.trading.algo.service;
 
 import com.trading.algo.strategy.*;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
-public class SignalStrategyFactory {
-    private static Map<Integer, SignalStrategy> strategyMap;
+import java.util.Properties;
 
+public class SignalStrategyFactory {
+    private static Map<Integer, SignalStrategy> strategyMap = new HashMap<>();
     //initialize the map lazily on the first call to get the strategy
     public static SignalStrategy getStrategy(int signal) {
-        if (strategyMap == null || strategyMap.isEmpty()) {
-            initMap();
+        if (strategyMap.isEmpty()) {
+            initStrategyMap();
         }
         SignalStrategy strategy = strategyMap.get(signal);
         if (strategy == null) {
@@ -24,14 +27,19 @@ public class SignalStrategyFactory {
         return strategy;
     }
 
-    private static synchronized void initMap() {
-        if (strategyMap == null) {
-            strategyMap = new HashMap<>();
-            strategyMap.put(1, new SetUpSignalStrategy());
-            strategyMap.put(2, new ReverseSignalStrategy());
-            strategyMap.put(3, new PerformCalcStrategy());
-            strategyMap.put(-1, new CancelTradesStrategy());
-            // Add more strategies as needed
-        }
+    private static synchronized void initStrategyMap() {
+            Properties prop = new Properties();
+            try (InputStream input = SignalStrategyFactory.class.getClassLoader().getResourceAsStream("config.properties")) {
+                prop.load(input);
+                strategyMap = new HashMap<>();
+                for (String key : prop.stringPropertyNames()) {
+                    int signal = Integer.parseInt(key);
+                    String className = prop.getProperty(key);
+                    SignalStrategy strategy = (SignalStrategy) Class.forName(className).newInstance();
+                    strategyMap.put(signal, strategy);
+                }
+            } catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+                throw new IllegalStateException("Failed to initialize signal strategy map", e);
+            }
     }
 }
